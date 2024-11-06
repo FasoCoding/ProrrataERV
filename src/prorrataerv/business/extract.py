08c1d0da9f2e10b62427afead1766653efac7889
FasoCoding <bastidas.felipe@google.com>
2024-11-06 08:53:39 -0300

    inicio nueva logica de extracción de datos
    se rehace de cero la capa lógica de obtención de datos. Se mantiene la
    versión original mientras se trabaja en la nueva.

--- src/prorrataerv/business/extract.py
+++ src/prorrataerv/business/extract.py
@@ -1,13 +1,11 @@
-import importlib.resources as sql_resources
+from prorrataerv.model.accdb import Querier
+from prorrataerv.model.accdb.schema import QuerySchema
+from prorrataerv.model.minstable import get_minstablelevel
+from prorrataerv.model.pmgds import get_pmgd_list
+from prorrataerv.model.banned import get_banned_list
 
 from pathlib import Path
-
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy import (
-    engine,
-    create_engine,
-)
-
+import sqlalchemy as sa
 import polars as pl
 
 class DataExtractor:
@@ -28,95 +26,22 @@ class DataExtractor:
     def extract_data(self) -> None:
         """Inicia proceso de extracción de datos.
         """
-        with create_prg_engine(self.path_prg).connect() as conn:
-            sql = sql_resources.files("prorrata.sql")
-            self.nodes = get_access_data((sql / "gen_node.sql").read_text(), conn)
-            self.gen = get_access_data((sql / "gen_data.sql").read_text(), conn)
-            self.cmg = get_access_data((sql / "cmg_data.sql").read_text(), conn)
-
-        self.pmgd = get_pmgd(self.path_pmgd)
-        self.banned = get_banned_generators(self.path_banned)
-
-    def check_curtailment(self) -> bool:
-        total_row, _ = self.cmg.shape
-        total_curtailment = (
-            self.gen
-            .filter(pl.col("property").eq("Capacity Curtailed"))
-            .select(pl.col("value").sum())
-            .item(0,0)
+        connection_string = (
+            r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
+            rf"DBQ={self.path_prg.as_posix()};"
+            r"ExtendedAnsiSQL=1;"
         )
-        
-        return total_row > 0 and total_curtailment > 0
+        connection_url = sa.engine.URL.create(
+            "access+pyodbc", query={"odbc_connect": connection_string}
+        )
 
-def create_prg_engine(path_prg: Path) -> engine.Engine:
-    """Creates a SQLAlchemy engine for a Microsoft Access database.
+        engine = sa.create_engine(connection_url)
+        with engine.connect() as conn:
+            pcp_solution = Querier(conn=conn)
+            self.nodes = pcp_solution.get_obj_relationship(collection_id=12)
+            self.gen = pcp_solution.get_property_t_data(query_enum=QuerySchema.GENERATOR.GENERATION, category_list=["Solar Farms"])
+            node_categories = [item.category_name for item in pcp_solution.get_category_list(category_type=22) if item.category_id != 22]
+            self.cmg = pcp_solution.get_property_data(query_enum=QuerySchema.NODE.PRICE, category_list=node_categories)
 
-    This function takes a path to a Microsoft Access database file and returns a SQLAlchemy engine
-    that can be used to interact with the database.
-
-    Args:
-        path_prg (Path): A pathlib.Path object representing the path to the .mdb or .accdb file.
-
-    Raises:
-        ValueError: If the provided path does not exist.
-
-    Returns:
-        engine.Engine: A SQLAlchemy engine object.
-    """
-    connection_string = (
-        r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
-        rf"DBQ={path_prg.as_posix()};"
-        r"ExtendedAnsiSQL=1;"
-    )
-    connection_url = engine.URL.create(
-        "access+pyodbc", query={"odbc_connect": connection_string}
-    )
-
-    return create_engine(connection_url)
-
-
-def get_access_data(sql_str: str, prg_engine: engine.Engine) -> pl.DataFrame:
-    """Wrapper function to read data from a Microsoft Access database.
-
-    Args:
-        sql_str (str): sql query to be executed.
-        prg_engine (engine.Engine): SQLAlchemy engine object.
-
-    Raises:
-        f: SQLAlchemyError if connection to database fails.
-
-    Returns:
-        pl.DataFrame: A polars DataFrame with the results of the query.
-    """
-    try:
-        return pl.read_database(query=sql_str, connection=prg_engine)
-    except SQLAlchemyError as e:
-        raise f"Error: {e}"
-
-
-def get_pmgd(path_pmgd: Path) -> pl.DataFrame:
-    """Extracts a list of PMGDs from an Excel file on W disc.
-
-    Returns:
-        pl.DataFrame: A polars DataFrame with the list of PMGDs.
-    """
-    return pl.read_excel(
-        source=path_pmgd.absolute(),
-        sheet_name="Hoja1",
-        xlsx2csv_options={"skip_empty_lines": True},
-        read_csv_options={"new_columns": ["Nombre_CDC", "Centrales"]},
-    )
-
-
-def get_banned_generators(path_banned: Path) -> pl.DataFrame:
-    """Extracts a list of banned generators from an Excel file on R disc.
-
-    Returns:
-        pl.DataFrame: A polars DataFrame with the list of banned generators.
-    """
-    return pl.read_excel(
-        source=path_banned.absolute(),
-        sheet_name="Hoja1",
-        xlsx2csv_options={"skip_empty_lines": True},
-        read_csv_options={"new_columns": ["Centrales", "Comment"]},
-    )
+        self.pmgd = get_pmgd_list(self.path_pmgd)
+        self.banned = get_banned_list(self.path_banned)
