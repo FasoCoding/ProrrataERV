a7fa6cc368a6ff225259fc470695612856a8be48
FasoCoding <bastidas.felipe@google.com>
2024-11-13 19:09:00 -0300

    update custom type
    actualizacion del nombre del custom types para mantener convención.

--- src/prorrataerv/model/banned.py
+++ src/prorrataerv/model/banned.py
@@ -3,17 +3,17 @@ from typing import TypeAlias
 
 import fastexcel
 
-banned_names: TypeAlias = str
+BannedNames: TypeAlias = str
 
 
-def get_banned_list(path_to_banned: Path) -> list[banned_names]:
+def get_banned_list(path_to_banned: Path) -> list[BannedNames]:
     """recupera una lista de centrales vetadas del proceso de la prorrata.
 
     Args:
         path_to_banned (Path): ruta al excel con centrales vetadas.
 
     Returns:
-        list[str]: lista de nombres plexos
+        list[BannedNames]: lista de nombres plexos
     """
     excel_reader = fastexcel.read_excel(path_to_banned.absolute())
     data_sheet = excel_reader.load_sheet_by_idx(0, use_columns="A")
--- src/prorrataerv/model/pmgds.py
+++ src/prorrataerv/model/pmgds.py
@@ -2,16 +2,16 @@ from pathlib import Path
 
 import fastexcel
 
-type pmgd_names = str
+type PMGDNames = str
 
-def get_pmgd_list(path_to_pmgd: Path) -> list[pmgd_names]:
+def get_pmgd_list(path_to_pmgd: Path) -> list[PMGDNames]:
     """recupera una lista de centrales vetadas del proceso de la prorrata.
 
     Args:
         path_to_banned (Path): ruta al excel con centrales pmgd.
 
     Returns:
-        list[str]: lista de nombres plexos
+        list[PMGDNames]: lista de nombres plexos
     """
     excel_reader = fastexcel.read_excel(path_to_pmgd.absolute())
     data_sheet = excel_reader.load_sheet_by_idx(0, use_columns="A")
