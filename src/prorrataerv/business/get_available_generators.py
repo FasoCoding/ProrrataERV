823bb86fde21ebc68c60777d7a88af81b6930764
FasoCoding <bastidas.felipe@google.com>
2024-11-13 19:10:30 -0300

    available generators
    se agrega modulo para encontrar la lista de centrales a poder usar en la
    prorrata.

---
+++ src/prorrataerv/business/get_available_generators.py
@@ -0,0 +1,12 @@
+from prorrataerv.model.banned import BannedNames
+from prorrataerv.model.pmgds import PMGDNames
+from prorrataerv.model.schemas import Generator
+
+def get_generators_list(path_to_accdb: str) -> list[Generator]:
+    pass
+
+def get_banned_generatores_list(path_to_excel: str) -> list[BannedNames]:
+    pass
+
+def get_pmgd_list(path_to_excel: str) -> list[PMGDNames]:
+    pass
