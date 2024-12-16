1e2960434db58bc41b077c15e6b39db883635e4c
FasoCoding <bastidas.felipe@google.com>
2024-12-16 12:31:07 -0300

    correccion sql
    se ajustan sql de categoria por cambios en los id de plexos 10

--- prorrata/sql/gen_node.sql
+++ prorrata/sql/gen_node.sql
@@ -4,4 +4,4 @@ SELECT
 FROM ((t_membership
 INNER JOIN t_object as node_obj ON t_membership.child_object_id = node_obj.object_id)
 INNER JOIN t_object as gen_obj ON t_membership.parent_object_id = gen_obj.object_id)
-WHERE node_obj.class_id = 22 AND gen_obj.class_id = 2 AND gen_obj.category_id IN (95, 96, 99, 100) AND t_membership.collection_id = 12;
+WHERE node_obj.class_id = 22 AND gen_obj.class_id = 2 AND gen_obj.category_id IN (98, 99, 102, 103) AND t_membership.collection_id = 12;
